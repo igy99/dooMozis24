2765acacf48795c989e6e25eb8c3a47dd86ec2c7
igy99 <73252218+igy99@users.noreply.github.com>
2024-06-18 21:17:03 +0200

    Add files via upload

---
+++ components/titleComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from '../styles/titleComponent.module.css'
+
+const TitleComponent = ({ subtitle }) => {
+    return (<>
+        <div className={styles.mainContainer}>
+            <div className={styles.holderTitle}>
+                <div className={styles.title}>Mozis.com</div>
+                <div className={styles.subtitle}>{subtitle}</div>
+            </div>
+        </div>
+    </>)
+}
+
+export default TitleComponent;
---
+++ pages/ProizvodiSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ProizvodiComponent from "../components/proizvodiComponent";
+
+const ProizvodiSection = () => {
+    return (
+        <>
+            <ProizvodiComponent />
+        </>
+    );
+};
+export default ProizvodiSection;
